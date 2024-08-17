fcbfc5d38307cd7c6c2ff1ed0859a8a6fbfea08c
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2024-08-17 11:13:44 +0530

    chages in chatting application

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,7 +30,7 @@ const Projects = () => {
                     <h5 className="card-title">{data.title}</h5>
                     <p className="card-text">{data.description}</p>
                     <a href={data.source} className="btn btn-primary ">
-                      Github Link
+                      Link
                     </a>
                   </div>
                 </div>
